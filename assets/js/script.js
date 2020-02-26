fb96d63e62f553bbcc1b7ea337c1be60a1713fec
Erik Åström <astromerik93@gmail.com>
2020-02-26 19:45:19 +0000

    added nasdaq api link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
+$(document).ready(function(){
 
-$.getJSON("https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=Yb1WqRaFvoKardzS_a3V", function(data){
+    $.getJSON("https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=Yb1WqRaFvoKardzS_a3V", function(data){
     console.log(data);
-});
+    });
 
+    // let chart = 
+    $.getJSON("https://www.quandl.com/api/v3/datasets/NASDAQOMX/NDX.json?api_key=Yb1WqRaFvoKardzS_a3V", function(nasdaq){
+        console.log(nasdaq);
+    });
+}); 
 
-// $(document).ready(function(){
+// 
 
 //     $(".fa-dot-circle").click(function(){
 //         alert("hey");
 //     });
-// });
+// 

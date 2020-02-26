f603e3f4cd498048b6e5c1041ddf2a02ba61be94
Erik Åström <astromerik93@gmail.com>
2020-02-26 15:31:55 +0000

    added API

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+$.getJSON("https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=Yb1WqRaFvoKardzS_a3V", function(data){
+    console.log(data);
+});
+
+
+// $(document).ready(function(){
+
+//     $(".fa-dot-circle").click(function(){
+//         alert("hey");
+//     });
+// });

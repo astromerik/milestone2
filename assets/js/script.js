7c3583bc2aa4e031d14f73af3b35a7e0fc4586f5
Erik Åström <astromerik93@gmail.com>
2020-03-11 16:11:05 +0000

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,9 +92,9 @@ function searchStock() {
 }
 
 
- function removePara(){
-     searchText.removeChild(placeHolderText);
- }
+//  function removePara(){
+//      searchText.removeChild(placeHolderText);
+//  }
 
     // $.getJSON("https://www.quandl.com/api/v3/datasets/NASDAQOMX/NDX.json?api_key=Yb1WqRaFvoKardzS_a3V", function(nasdaq){
     //     console.log(nasdaq);
